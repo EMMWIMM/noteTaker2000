c29a0616f829b3f3966112e70a92e9374be56073
Emmalee <emilee.wimmer@gmail.com>
2022-10-03 15:25:09 -0600

    added helpers

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const path = require('path');
-const termData = require('./db/terms.json');
+const termData = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
-var randomId = require('random-id');
-const api = require('./public/assets/js/index.js')
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
+// var randomId = require('random-id');
+// const api = require('./public/assets/js/index.js')
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+// app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -22,6 +23,12 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/api/notes', (req, res) =>{
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  // res.sendfile( path.join(__dirname, '/db/db.json'))
+});
+
 
 
 // listening to port
